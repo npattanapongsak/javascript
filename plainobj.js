4e9e1fdb05a7ad433455f56a123c9dbb8f58f51a
Natthakorn Pattanapongsak <npattanapongsak@gmail.com>
2015-11-09 21:33:07 -0800

    Create plainobj.js

---
+++ plainobj.js
@@ -0,0 +1,55 @@
+var a = {
+    "string":"it is string",
+    "array":[1,2,3],
+    "object":{name:1},
+    "arrayInObj":{array:[4,5,6]},
+    "objInArray": [{obj:2}]
+}
+
+var expected = {
+  "string":"it is string",
+  "array[0]":1,
+  "array[1]":2,
+  "array[2]":3,
+  "object.name":1,
+  "arrayInObj.array[0]":4,
+  "arrayInObj.array[1]":5,
+  "arrayInObj.array[2]":6,
+  "objInArray[0].obj": 2 
+}
+function helper(key,value,result){
+  if(Object.prototype.toString.call(value)==='[object Array]'){
+    var i =0;
+    for(i=0;i<value.length;i++){
+      helper(key+"["+i+"]",value[i],result);
+    }
+  }else if(typeof value ==="object" ){
+    for(var name in value){
+      if(value.hasOwnProperty(name)){
+        helper(key+"."+name,value[name],result);  
+      }
+    }
+  }else{    
+    result[key]=value;  
+  }
+}
+function plainObj(obj){
+  var result ={};
+  for(var key in obj){
+    if(obj.hasOwnProperty(key)){
+      helper(key,obj[key],result);    
+    }
+  }
+  return result;
+}
+function compare(expected, result){
+  for(var key in expected){
+      if(expected.hasOwnProperty(key)){
+        if(!result.hasOwnProperty(key) || result[key] !== expected[key]){   
+          return false;
+        }
+      }
+  }
+  return true;
+}
+console.log(compare(expected,plainObj(a)));
